682aa54c3e5899dd138fc231a7e153d8a215e5a0
Mickiewicz <katarzyna.mickiewicz@accenture.com>
2019-11-14 11:22:21 +0100

    Detect face mood without taking photo

--- app/src/main/java/com/example/moodapp/test_me_activities/faceMoodActivity/FaceMoodActivity.java
+++ app/src/main/java/com/example/moodapp/test_me_activities/faceMoodActivity/FaceMoodActivity.java
@@ -15,8 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.example.moodapp.R;
-import com.example.moodapp.test_me_activities.faceMoodActivity.camera.CameraOverlay;
-import com.example.moodapp.test_me_activities.faceMoodActivity.camera.CameraSurfacePreview;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.vision.CameraSource;
@@ -28,302 +26,187 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 
-
 public class FaceMoodActivity extends AppCompatActivity {
 
     private static final String TAG = "FaceTrackerDemo";
-
     private CameraSource mCameraSource = null;
-
     private CameraSurfacePreview mPreview;
-
     private CameraOverlay cameraOverlay;
-
     private static final int RC_HANDLE_GMS = 9001;
-
     private static final int RC_HANDLE_CAMERA_PERM = 2;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
-        mPreview = (CameraSurfacePreview) findViewById(R.id.preview);
-
-        cameraOverlay = (CameraOverlay) findViewById(R.id.faceOverlay);
-
+        setContentView(R.layout.activity_face_mood);
+        mPreview = findViewById(R.id.preview);
+        cameraOverlay = findViewById(R.id.faceOverlay);
         int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-
         if (rc == PackageManager.PERMISSION_GRANTED) {
-
             createCameraSource();
-
         } else {
-
             requestCameraPermission();
-
         }
-
     }
+
 
     private void requestCameraPermission() {
 
         final String[] permissions = new String[]{Manifest.permission.CAMERA};
 
         if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-
                 Manifest.permission.CAMERA)) {
-
             ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
-
             return;
-
         }
 
         final Activity thisActivity = this;
 
         View.OnClickListener listener = new View.OnClickListener() {
-
             @Override
-
             public void onClick(View view) {
-
                 ActivityCompat.requestPermissions(thisActivity, permissions,
-
                         RC_HANDLE_CAMERA_PERM);
-
             }
-
         };
 
         Snackbar.make(cameraOverlay, "Camera permission is required",
-
                 Snackbar.LENGTH_INDEFINITE)
-
                 .setAction("OK", listener)
-
                 .show();
-
     }
+
 
     private void createCameraSource() {
 
         Context context = getApplicationContext();
-
         FaceDetector detector = new FaceDetector.Builder(context)
-
                 .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
-
                 .build();
 
         detector.setProcessor(
-
-                new MultiProcessor.Builder<>(new  FaceMoodActivity.GraphicFaceTrackerFactory())
-
-
+                new MultiProcessor.Builder<>(new FaceMoodActivity.GraphicFaceTrackerFactory())
                         .build());
 
         if (!detector.isOperational()) {
 
             Log.e(TAG, "Face detector dependencies are not yet available.");
-
         }
 
         mCameraSource = new CameraSource.Builder(context, detector)
-
                 .setRequestedPreviewSize(640, 480)
-
                 .setFacing(CameraSource.CAMERA_FACING_FRONT)
-
                 .setRequestedFps(30.0f)
-
                 .build();
-
     }
 
     @Override
-
     protected void onResume() {
-
         super.onResume();
 
         startCameraSource();
-
     }
 
     @Override
-
     protected void onPause() {
-
         super.onPause();
-
         mPreview.stop();
-
     }
 
     @Override
-
     protected void onDestroy() {
-
         super.onDestroy();
-
         if (mCameraSource != null) {
-
             mCameraSource.release();
-
         }
-
     }
 
     @Override
-
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-
         if (requestCode != RC_HANDLE_CAMERA_PERM) {
-
             Log.d(TAG, "Got unexpected permission result: " + requestCode);
-
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
             return;
-
         }
 
         if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
             Log.d(TAG, "Camera permission granted - initialize the camera source");
-
             createCameraSource();
-
             return;
-
         }
 
         Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
-
                 " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
 
         DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-
             public void onClick(DialogInterface dialog, int id) {
-
                 finish();
-
             }
-
         };
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
         builder.setTitle("FaceTrackerDemo")
-
                 .setMessage("Need Camera access permission!")
-
                 .setPositiveButton("OK", listener)
-
                 .show();
-
     }
 
+
     private void startCameraSource() {
-
         int code = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(
-
                 getApplicationContext());
-
         if (code != ConnectionResult.SUCCESS) {
-
             Dialog dlg =
-
                     GoogleApiAvailability.getInstance().getErrorDialog(this, code, RC_HANDLE_GMS);
-
             dlg.show();
-
         }
 
         if (mCameraSource != null) {
-
             try {
-
                 mPreview.start(mCameraSource, cameraOverlay);
-
             } catch (IOException e) {
-
                 Log.e(TAG, "Unable to start camera source.", e);
-
                 mCameraSource.release();
-
                 mCameraSource = null;
-
             }
-
         }
-
     }
 
     private class GraphicFaceTrackerFactory implements MultiProcessor.Factory<Face> {
-
         @Override
-
         public Tracker<Face> create(Face face) {
-
             return new FaceMoodActivity.GraphicFaceTracker(cameraOverlay);
-
         }
-
     }
 
     private class GraphicFaceTracker extends Tracker<Face> {
-
         private CameraOverlay mOverlay;
-
         private FaceOverlayGraphics faceOverlayGraphics;
 
         GraphicFaceTracker(CameraOverlay overlay) {
-
             mOverlay = overlay;
-
             faceOverlayGraphics = new FaceOverlayGraphics(overlay);
-
         }
 
         @Override
-
         public void onNewItem(int faceId, Face item) {
-
             faceOverlayGraphics.setId(faceId);
-
         }
 
         @Override
-
         public void onUpdate(FaceDetector.Detections<Face> detectionResults, Face face) {
-
             mOverlay.add(faceOverlayGraphics);
-
             faceOverlayGraphics.updateFace(face);
-
         }
 
         @Override
-
         public void onMissing(FaceDetector.Detections<Face> detectionResults) {
-
             mOverlay.remove(faceOverlayGraphics);
-
         }
 
         @Override
-
         public void onDone() {
-
             mOverlay.remove(faceOverlayGraphics);
-
         }
-
     }
-
 }
